bbebf28db5729c5d16f42d84859724a4873b1b5d
Esdras Neres Carneiro <74979135+EsdrasNC@users.noreply.github.com>
2021-06-27 17:18:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,94 @@
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+function changeHeaderWhenScrol() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+
+const swiper = new Swiper('.swiper-container', {
+  slidePerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+scrollReveal.reveal(
+  `#home .text, #home .image,
+  #About .text, #About .image,
+  #Services header, #Services .card,
+  #Testimonials header, #Testimonials .Testimonials,
+  #Contact .text, #Contact .links,
+  footer .brand, footer .social
+`,
+  { interval: 100 }
+)
+
+const backTopButton = document.querySelector('.backtop')
+
+function backTop() {
+  if (window.scrollY >= 560) {
+    backTopButton.classList.add('show')
+  } else {
+    backTopButton.classList.remove('show')
+  }
+}
+
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScrol()
+  backTop()
+  activateMenuAtCurrentSection()
+})
